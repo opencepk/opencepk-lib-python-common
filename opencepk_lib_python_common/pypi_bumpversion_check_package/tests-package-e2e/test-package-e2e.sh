a9f9cb2a74c2b9e84782eabc1699f713f7a5fbe3
hminaee-tc <hminaee@tucows.com>
2024-07-03 10:13:37 -0300

    feat/check-v2 clean up

--- opencepk_lib_python_common/pypi_bumpversion_check_package/tests-package-e2e/test-package-e2e.sh
+++ opencepk_lib_python_common/pypi_bumpversion_check_package/tests-package-e2e/test-package-e2e.sh
@@ -6,8 +6,6 @@ TEST_DIR="tests-package-e2e"
 # Run the python package
 #-------------------------------
 echo "Running in: $(pwd)"
-# (cd .. && python find_and_replace_strings/main.py --config ${TEST_DIR}/.find-and-replace.json ${TEST_DIR}/${TEST_TARGET_FILE})
 pushd .. > /dev/null
-pwd
 python3 pypi_bumpversion_check/main.py './${TEST_DIR}/pyproject.test'
 popd > /dev/null
