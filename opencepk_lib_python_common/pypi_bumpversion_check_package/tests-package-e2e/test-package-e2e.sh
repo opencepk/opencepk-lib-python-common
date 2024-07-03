0fdc7fde6215905981a1b5250c757cc184f91d50
hminaee-tc <hminaee@tucows.com>
2024-07-02 23:38:49 -0300

    feat/check-v2 add e2e for check v

--- opencepk_lib_python_common/pypi_bumpversion_check_package/tests-package-e2e/test-package-e2e.sh
+++ opencepk_lib_python_common/pypi_bumpversion_check_package/tests-package-e2e/test-package-e2e.sh
@@ -11,4 +11,5 @@ TEST_TARGET_FILE="README_TEST_PACKAGE.md"
 echo "${TEST_SCRIPT_NAME}: Running pre-commit using ${TEST_DIR}/.pre-commit-config.yaml"
 # (cd .. && python find_and_replace_strings/main.py --config ${TEST_DIR}/.find-and-replace.json ${TEST_DIR}/${TEST_TARGET_FILE})
 cd ..
-python3 pypi_bumpversion_check/main.py './opencepk_lib_python_common/pypi_bumpversion_check_package/tests-pre-commit-hook/pyproject.test'
+pwd
+python3 pypi_bumpversion_check/main.py './tests-package-e2e/pyproject.test'
